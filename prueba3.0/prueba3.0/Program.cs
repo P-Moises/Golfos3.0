6635c2aa6d64e95789be4dea5d652f79ee3f763f
P-Moises <gtpmoises@gmail.com>
2026-09-06 11:39:20 -0400

    init
    commi

---
+++ prueba3.0/prueba3.0/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace prueba3._0
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
